d85893b5cffb229122d6db27c6ecf5160eef33c2
Lappy_EEng <tigz@EEng-Rig.com>
2025-10-02 17:13:31 -0700

    Fixed vhdl codes & finished Part 2 as well

--- SourceCode/OddParity.vhd
+++ SourceCode/OddParity.vhd
@@ -2,7 +2,7 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity OddParity is
-  generic ( N : natural := 8 );
+  generic ( N : natural := 7 );
   port (
     X     : in  std_logic_vector(N-1 downto 0);
     IsOdd : out std_logic
--- SourceCode/oddparity_chain.vhd
+++ SourceCode/oddparity_chain.vhd
@@ -2,17 +2,26 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 architecture CHAIN of OddParity is
-  signal s : std_logic_vector(N downto 0);
+  signal s : std_logic_vector(N downto 1);
 begin
-  s(0) <= '0';
+  -- first XOR uses X(0) and X(1)
+  first_gate : entity work.XOR2(rtl)
+    port map (
+      A => X(0),
+      B => X(1),
+      Y => s(1)
+    );
 
-  g_chain : for i in 0 to N-1 generate
-    -- use XOR2 primitive OR the built-in xor; both are fine
-    -- instantiate:
-    -- gate_i : entity work.XOR2(rtl) port map (A => s(i), B => X(i), Y => s(i+1));
-    -- or simpler operator:
-    s(i+1) <= s(i) xor X(i);
+  -- rest of the chain from bit 2 onward
+  g_chain : for i in 2 to N-1 generate
+    gate_i : entity work.XOR2(rtl)
+      port map (
+        A => s(i-1),
+        B => X(i),
+        Y => s(i)
+      );
   end generate;
 
-  IsOdd <= s(N);
+  IsOdd <= s(N-1);
 end architecture CHAIN;
+
